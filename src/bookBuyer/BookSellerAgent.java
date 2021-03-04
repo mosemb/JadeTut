247cafd8630eb3c3d51ebdb8631e9d9839c09840
mosemb <mosejava@gmail.com>
2021-03-04 23:15:32 +0100

    Book Buyer2

--- src/bookBuyer/BookSellerAgent.java
+++ src/bookBuyer/BookSellerAgent.java
@@ -1,5 +1,164 @@
 package bookBuyer;
 
-public class BookSellerAgent {
+import java.util.Hashtable;
 
+
+import jade.content.onto.basic.Action;
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+public class BookSellerAgent extends Agent{
+	
+	// Catalogue of books 
+	private Hashtable catalogue; 
+	
+	// Gui to store the books 
+	private BookSellerGui myGui; 
+	
+	// Agent intializations 
+	public void setup() {
+		
+		// Create the new catalogue 
+		catalogue = new Hashtable();
+		
+		// Create GUI 
+		myGui = new BookSellerGui(this);
+		
+		// Register the book serving example in the Directory Facilitor
+		DFAgentDescription dfd = new DFAgentDescription(); 
+		dfd.setName(getAID());
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType("book-selling");
+		sd.setName("Jade-book-trading");
+		dfd.addServices(sd);
+		
+		try {
+			
+			DFService.register(this, dfd);
+			
+		} catch ( FIPAException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		// Behavior of buyers inquiring about book sales
+		addBehaviour(new PurchaseOrderServer());
+		
+		// Fulfilling purchase orders from sellers 
+		addBehaviour(new OfferRequestsServer());
+	}
+	
+	/*
+	 * Adding books to the catalogue using a oneShotBehavior
+	 * */
+	public void updateCatalogue(final String title, final int price) {
+		 
+		addBehaviour(new OneShotBehaviour() {
+			
+			@Override
+			public void action() {
+				// TODO Auto-generated method stub
+				catalogue.put(title, new Integer(price));
+				System.out.println(title + " Added into the catalogue with price "+ price);
+				
+			}
+		});
+	}
+	
+	// Agent Clean up. 
+	public void takeDown() {
+		// This is the degregestration 
+		try {
+			
+			DFService.deregister(this);
+			
+			
+			
+		} catch (FIPAException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		// Dispose off the GUI
+		myGui.dispose();
+		
+		System.out.println("Seller Agent ID "+getAID().getName()+ " is terminating.");
+		
+	}
+	
+	
+	
+	/*
+	 * This class accepts messages from buyers inquiring about the books that are available
+	 * For sale. And then 
+	 * 
+	 * */
+	public class OfferRequestsServer extends CyclicBehaviour{	
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP);
+			ACLMessage msg = myAgent.receive(mt);
+			
+			if(msg!=null) {
+				String title = msg.getContent(); 
+				ACLMessage reply = msg.createReply(); 
+				Integer price = (Integer) catalogue.get(title);
+				if(price != null) {
+					// The requested book is available for sale then reply with a message
+					reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(String.valueOf(price.intValue()));
+				}else {
+					reply.setPerformative(ACLMessage.REFUSE);
+					reply.setContent("Not available");
+				}
+				myAgent.send(reply);
+			}else {
+				block();
+			}
+
+		}
+		
+	}
+	/*
+	 * This class fulfills purchase orders from buyers and the title of the book is removed from 
+	 * the catalog after a sale. 
+	 * */
+	
+	private class PurchaseOrderServer extends CyclicBehaviour {
+		
+		public void action() {
+			MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL);
+			ACLMessage msg = myAgent.receive(mt);
+			if(msg!=null) {
+				// Accept proposal message and process it. 
+				String title = msg.getContent(); 
+				ACLMessage reply = msg.createReply();
+				
+				Integer price = (Integer) catalogue.remove(title);
+				
+				if(price!= null) {
+					reply.setPerformative(ACLMessage.INFORM);
+					
+					System.out.println(title + " sold to agent "+msg.getSender().getName());
+				}else {
+					reply.setPerformative(ACLMessage.FAILURE);
+					reply.setContent("not - available");
+				}
+				myAgent.send(reply);
+				
+			} else {
+				
+				block();
+			}
+			
+		}
+		
+	}
+	
 }
--- src/bookBuyer/BookSellerGui.java
+++ src/bookBuyer/BookSellerGui.java
@@ -2,38 +2,70 @@ package bookBuyer;
 
 import jade.core.AID;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.print.attribute.standard.PDLOverrideSupported;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import com.sun.tools.sjavac.comp.dependencies.PublicApiCollector;
 
 
 public class BookSellerGui extends JFrame {
 	
 	private BookSellerAgent myAgent;
-	private JTextField priceField, textField; 
+	private JTextField priceField, textField;
+	private JTextField titleField; 
 	
 	public BookSellerGui(BookSellerAgent a) {
 		// TODO Auto-generated constructor stub
 		super(a.getLocalName());
-		myAgent = a;
+		this.myAgent = a;
 		
 		JPanel p = new JPanel();
 		p.setLayout(new GridLayout(2,2));
+		p.add(new JLabel("Book title"));
+		titleField = new JTextField(15); 
+		p.add(titleField); 
+		p.add(new JLabel("Price"));
+		priceField = new JTextField(15); 
+		p.add(priceField); 
+		getContentPane().add(p,BorderLayout.CENTER);
+		
+		JButton addButton = new JButton("Add");
+		addButton.addActionListener( new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				try {
+					
+					String title = titleField.getText().trim(); 
+					String price = priceField.getText().trim();
+					myAgent.updateCatalogue(title, Integer.parseInt(price));
+					titleField.setText("");
+					priceField.setText("");
+						
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+				
+			}
+		}
+				
+			    
+				);
 		
 		
+	
 		
-		
-		JPanel p = new JPanel();
-		p.setLayout(new GridLayout(2, 2));
-		p.add(new JLabel("Book title:"));
-		titleField = new JTextField(15);
-		p.add(titleField);
-		p.add(new JLabel("Price:"));
-		priceField = new JTextField(15);
-		p.add(priceField);
-		getContentPane().add(p, BorderLayout.CENTER);
+	
 		
 	}
 
